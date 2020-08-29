aa2ee5e3567d7a6fa1e1ae7fab404fd309ccecf6
camiloneiza1 <camiloneiza1@gmail.com>
2020-08-29 16:52:15 -0500

    Primer commit buffet

---
+++ src/main/java/co/edu/ucentral/buffet/security/DataBaseWebSecurity.java
@@ -0,0 +1,39 @@
+package co.edu.ucentral.buffet.security;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class DataBaseWebSecurity extends WebSecurityConfigurerAdapter{
+	@Autowired
+	private DataSource dataSourse;
+	
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.jdbcAuthentication().dataSource(dataSourse).usersByUsernameQuery("SELECT username, password, estatus FROM"
+				+ " Usuarios where username = ?")
+		.authoritiesByUsernameQuery("SELECT u.username, p.perfil FROM UsuarioPerfil up "
+				+ "INNER JOIN Usuarios u ON u.idUsuarios = up.idUsuario "
+				+ "INNER JOIN Perfiles p ON p.idPerfiles = up.idPerfil where u.username = ? ");
+	}
+	
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers(
+				"/js/**",
+				"/css/**",
+				"/fonts/**",
+				"/images/**",
+				"/tinymce/**").permitAll()
+		.antMatchers(
+				"/",
+				"/search/**").permitAll()
+		.anyRequest().authenticated()
+		.and().formLogin().permitAll();
+	}
+}
